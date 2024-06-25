67233527acd9cc43cd44231396fa638cc4059401
cyrineKc <cyrine.kchir92@gmail.com>
2024-06-25 22:56:24 +0200

    first commit

---
+++ MyPostsBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPostsBasic
+//
+//  Created by Cyrine Kchir on 25.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPostsBasic/MyPostsBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPostsBasicApp.swift
+//  MyPostsBasic
+//
+//  Created by Cyrine Kchir on 25.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MyPostsBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
